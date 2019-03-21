60f3da77bafa801490247f3508d0b2167d3d0d2f
timquiatchon <timothy-felix.lopez.quiatchon@cieep.com>
2019-03-21 13:48:27 +0100

    primera version

---
+++ src/ejemplonetbeansgit/EjemploNetbeansGit.java
@@ -0,0 +1,12 @@
+
+package ejemplonetbeansgit;
+
+
+public class EjemploNetbeansGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hi world");
+        System.out.println("ESTAS FELIZ ?");
+    }
+    
+}
